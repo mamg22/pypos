3d9799d6a0a154d02dda539053f6ad96baa5fc4d
mamg22 <45301823+mamg22@users.noreply.github.com>
2024-11-16 15:47:32 -0400

    Add base cart widget

--- src/pypos/cart.py
+++ src/pypos/cart.py
@@ -1,11 +1,106 @@
-from PySide6 import QtWidgets
+from decimal import Decimal
+
+from PySide6 import QtCore, QtSql, QtWidgets
+from PySide6.QtCore import Qt
+
+
+class CartTable(QtWidgets.QTableWidget):
+    CART_QUERY = """\
+    SELECT p.id, name, quantity, sell_currency, sell_value
+    FROM Cart c
+        INNER JOIN Products p
+        ON c.product = p.id
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        header_labels = ["Item", "Precio unitario", "Unidades", "Total"]
+        self.setColumnCount(len(header_labels))
+        self.setHorizontalHeaderLabels(header_labels)
+
+        self.setSelectionBehavior(
+            QtWidgets.QAbstractItemView.SelectionBehavior.SelectRows
+        )
+        self.setSelectionMode(QtWidgets.QAbstractItemView.SelectionMode.SingleSelection)
+        self.horizontalHeader().setSectionResizeMode(
+            QtWidgets.QHeaderView.ResizeMode.Fixed
+        )
+        self.horizontalHeader().setSectionResizeMode(
+            0, QtWidgets.QHeaderView.ResizeMode.Stretch
+        )
+        self.verticalHeader().setSectionResizeMode(
+            QtWidgets.QHeaderView.ResizeMode.Fixed
+        )
+        self.verticalHeader().hide()
+
+        self.refresh()
+
+    @QtCore.Slot()
+    def refresh(self) -> None:
+        db = QtSql.QSqlDatabase.database()
+        query = QtSql.QSqlQuery()
+
+        query.prepare(self.CART_QUERY)
+
+        if not query.exec():
+            print(query.lastError())
+            return
+
+        if db.driver().hasFeature(QtSql.QSqlDriver.DriverFeature.QuerySize):
+            n_rows = query.size()
+        else:
+            query.last()
+            n_rows = max(query.at() + 1, 0)
+            query.seek(QtSql.QSql.Location.BeforeFirstRow.value)
+
+        self.setRowCount(n_rows)
+
+        ItemFlag = Qt.ItemFlag
+        row_flags = ItemFlag.ItemIsSelectable | ItemFlag.ItemIsEnabled
+        number_align = Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignRight
+
+        for row_num in range(n_rows):
+            query.next()
+            row_id, name, quantity, sell_currency, int_sell_value = (
+                query.value(i) for i in range(query.record().count())
+            )
+            sell_value = Decimal(int_sell_value) / 100
+
+            base_item = QtWidgets.QTableWidgetItem()
+            base_item.setFlags(row_flags)
+            base_item.setData(Qt.ItemDataRole.UserRole, row_id)
+
+            name_item = base_item.clone()
+            name_item.setText(name)
+
+            quantity_item = base_item.clone()
+            quantity_item.setText(str(quantity))
+            quantity_item.setTextAlignment(number_align)
+
+            currency_symbol = "$" if sell_currency == "USD" else "Bs"
+
+            unit_item = base_item.clone()
+            unit_item.setText(f"{currency_symbol} {sell_value:.2f}")
+            unit_item.setTextAlignment(number_align)
+
+            total_value = sell_value * quantity
+
+            total_item = base_item.clone()
+            total_item.setText(f"{currency_symbol} {total_value:.2f}")
+            total_item.setTextAlignment(number_align)
+
+            for idx, item in enumerate(
+                (name_item, unit_item, quantity_item, total_item)
+            ):
+                self.setItem(row_num, idx, item)
 
 
 class CartWidget(QtWidgets.QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        self.cart_table = QtWidgets.QTableWidget()
+        self.cart_table = CartTable()
 
         main_layout = QtWidgets.QVBoxLayout()
 
